f4a52701f91d77a78141fdaa632f9a89e1120ea7
Youssef Ghareb <youssef.ghareb99@gmail.com>
2022-11-21 14:20:40 +0200

    Init

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=pass1234
+export POSTGRES_HOST=database-1.clluo8smo43v.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=database-1
+export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://udagram-yg.s3-website-us-east-1.amazonaws.com 
